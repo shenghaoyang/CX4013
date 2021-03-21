0c8e3d769a53b40569a22354bc3481c5fa96c4df
Shenghao Yang <me@shenghaoyang.info>
2021-03-21 14:56:18 +0800

    serialization/welknown: add void/error union type

--- serialization/wellknown.py
+++ serialization/wellknown.py
@@ -5,10 +5,14 @@ Well-known types that are not part of the core RPC types.
 """
 
 
-from serialization.derived import create_struct_type
+from serialization.derived import create_struct_type, create_union_type, String
 from typing import Final
 
 
 # Void type -> 0 size, empty structure.
 Void: Final = create_struct_type("Void", ())
 
+# Union, Void or an error string. Useful for reporting errors.
+VoidOrError: Final = create_union_type(
+    "VoidOrError", (("void", Void), ("error", String))
+)
