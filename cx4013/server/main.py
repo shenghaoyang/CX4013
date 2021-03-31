shenghaoyang/CX4013

commit b16c01a4068250e836c4104a543dde12f33a0e3d

treewide: refactor and reformat

cx4013/server/main.py
@@ -29,12 +29,12 @@ import logging
 import asyncio
 import argparse
 from pathlib import Path
-from rpc.common import DEFAULT_PORT
-from rpc.protocol import AddressType
-from rpc.helpers import create_server
-from rpc.skeleton import generate_skeleton, Skeleton
-from server.bookingserver import BookingServerImpl
-from server.bookingtable import Table
+from cx4013.rpc.common import DEFAULT_PORT
+from cx4013.rpc.protocol import AddressType
+from cx4013.rpc.helpers import create_server
+from cx4013.rpc.skeleton import generate_skeleton, Skeleton
+from cx4013.server.bookingserver import BookingServerImpl
+from cx4013.server.bookingtable import Table
 from collections.abc import Sequence
 
 
@@ -72,7 +72,7 @@ async def main(args: Sequence[str]) -> int:
 
     # Parse the arguments.
     try:
-        args = parser.parse_args(args[1:])
+        parsed = parser.parse_args(args[1:])
     except argparse.ArgumentError:
         return 1
 
@@ -82,7 +82,7 @@ async def main(args: Sequence[str]) -> int:
 
     # Load the bookings database.
     db = Path("bookings.sqlite")
-    if (f := args.reinit_facilities) is not None:
+    if (f := parsed.reinit_facilities) is not None:
         logger.info("db: reinitializing booking database")
         try:
             names = set(f for f in f.read().split("\n") if len(f) and not f.isspace())
@@ -101,7 +101,7 @@ async def main(args: Sequence[str]) -> int:
     # Factory function that returns a skeleton to use for every new connection.
     # Return a skeleton bound to a new server object for every new connection.
     sobjects: dict[AddressType, BookingServerImpl] = dict()
-    sobject_set = set()
+    sobject_set: set[BookingServerImpl] = set()
 
     def skel_fac(addr: AddressType) -> Skeleton:
         # Create the server object.
@@ -118,23 +118,30 @@ async def main(args: Sequence[str]) -> int:
         del sobjects[addr]
         logger.info(f"server: client {addr} disconnected")
 
-    logger.info(f"server: listening at {args.laddr}:{args.lport}")
+    logger.info(f"server: listening at {parsed.laddr}:{parsed.lport}")
     logger.info(
-        f"server: inactivity timeout: {args.itimeout}s, AMO entry timeout: {args.etimeout}s"
+        f"server: inactivity timeout: {parsed.itimeout}s, AMO entry timeout: {parsed.etimeout}s"
     )
 
     # Create the server and wait for it to be up.
     s = await create_server(
-        (args.laddr, args.lport),
+        (parsed.laddr, parsed.lport),
         skel_fac,
         disconnect_callback,
-        args.itimeout,
-        args.etimeout,
+        parsed.itimeout,
+        parsed.etimeout,
     )
-    # Server for one hour.
-    await asyncio.sleep(3600)
+    # Serve continuously.
+    loop = asyncio.get_running_loop()
+    loop.run_forever()
     s.stop()
+
+    return 0
+
+
+def main_wrapper():
+    exit(asyncio.run(main(sys.argv)))
 
 
 if __name__ == "__main__":
-    exit(asyncio.run(main(sys.argv)))
+    main_wrapper()

cx4013/client/main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """
 main.py
 
@@ -13,14 +15,14 @@ from collections.abc import Sequence
 from prompt_toolkit import print_formatted_text as print, HTML
 from prompt_toolkit.shortcuts import clear
 
-from client.notificationserver import BookingNotificationServerImpl
-from client.repl import Repl
-from client.hooks import RandomRequestReplyDropper
-from rpc.common import DEFAULT_PORT
-from rpc.helpers import create_and_connect_client, create_server
-from rpc.protocol import AddressType
-from rpc.skeleton import generate_skeleton, Skeleton
-from server.bookingserver import BookingServerProxy
+from cx4013.client.notificationserver import BookingNotificationServerImpl
+from cx4013.client.repl import Repl
+from cx4013.client.hooks import RandomRequestReplyDropper
+from cx4013.rpc.common import DEFAULT_PORT
+from cx4013.rpc.helpers import create_and_connect_client, create_server
+from cx4013.rpc.protocol import AddressType
+from cx4013.rpc.skeleton import generate_skeleton, Skeleton
+from cx4013.server.bookingserver import BookingServerProxy
 
 
 async def main(args: Sequence[str]) -> int:
@@ -69,7 +71,7 @@ async def main(args: Sequence[str]) -> int:
         pass
 
     # Create notification server.
-    s = await create_server(
+    _ = await create_server(
         (args.caddr, args.cport),
         skel_fac,
         disconnect_callback,
@@ -101,5 +103,9 @@ async def main(args: Sequence[str]) -> int:
         c.close()
 
 
-if __name__ == "__main__":
+def main_wrapper():
     exit(asyncio.run(main(sys.argv)))
+
+
+if __name__ == "__main__":
+    main_wrapper()

cx4013/client/hooks.py
@@ -7,7 +7,7 @@ Client-side pre-receive and pre-send hooks used to simulate packet loss.
 
 import random
 from typing import Optional
-from rpc.packet import PacketFlags, PacketHeader
+from cx4013.rpc.packet import PacketFlags, PacketHeader
 
 
 class RandomRequestReplyDropper:

cx4013/client/notificationserver.py
@@ -5,12 +5,12 @@ Implementation of the booking notification service.
 """
 
 
-from rpc.common import RemoteInterface, remotemethod
-from rpc.proxy import generate_proxy
-from serialization.derived import create_enum_type
-from serialization.numeric import u64
-from serialization.wellknown import Void
-from server.apptypes import ArrayTimeRange, String, rpc_tr_as_dtrange
+from cx4013.rpc.common import RemoteInterface, remotemethod
+from cx4013.rpc.proxy import generate_proxy
+from cx4013.serialization.derived import create_enum_type
+from cx4013.serialization.numeric import u64
+from cx4013.serialization.wellknown import Void
+from cx4013.server.apptypes import ArrayTimeRange, String, rpc_tr_as_dtrange
 
 Action = create_enum_type("Action", ("CREATE", "RELEASE", "MODIFY"))
 

cx4013/client/repl.py
@@ -15,15 +15,15 @@ from prompt_toolkit.shortcuts import clear
 from prompt_toolkit.validation import Validator, ValidationError
 from tabulate import tabulate
 
-from rpc.protocol import RPCClient
-from rpc.packet import InvocationSemantics
-from client.notificationserver import BookingNotificationServerImpl
-from client.hooks import RandomRequestReplyDropper
-from serialization.derived import String
-from serialization.numeric import u8, u32, u64
-from server.bookingserver import BookingServerProxy
-from server.bookingtable import START_DATE
-from server.apptypes import (
+from cx4013.rpc.protocol import RPCClient
+from cx4013.rpc.packet import InvocationSemantics
+from cx4013.client.notificationserver import BookingNotificationServerImpl
+from cx4013.client.hooks import RandomRequestReplyDropper
+from cx4013.serialization.derived import String
+from cx4013.serialization.numeric import u8, u32, u64
+from cx4013.server.bookingserver import BookingServerProxy
+from cx4013.server.bookingtable import START_DATE
+from cx4013.server.apptypes import (
     DayOfWeek,
     ArrayDayOfWeek,
     rpc_tr_as_dtrange,
@@ -193,7 +193,7 @@ class Repl:
                 components = s.split("-")
                 if len(components) != 2:
                     raise ValidationError(
-                        0, f"expecting two components separated by a dash (-)"
+                        0, "expecting two components separated by a dash (-)"
                     )
 
         return await self._session.prompt_async(
@@ -228,23 +228,23 @@ class Repl:
                 s = document.text
                 components = s.split(":")
                 if len(components) != 2:
-                    raise ValidationError(0, f"expecting (+/-)HH:MM")
+                    raise ValidationError(0, "expecting (+/-)HH:MM")
 
                 c1, c2 = components
                 pc1, pc2 = s.find(c1), s.rfind(c2)
                 if c1[0] not in ("+", "-"):
                     raise ValidationError(
-                        0, f"expecting +/- before HH:MM for shift direction"
+                        0, "expecting +/- before HH:MM for shift direction"
                     )
 
                 if not c1[1:].isnumeric():
                     raise ValidationError(
-                        pc1, f"expecting hour shift component to be an integer"
+                        pc1, "expecting hour shift component to be an integer"
                     )
 
                 if not c2.isnumeric():
                     raise ValidationError(
-                        pc2, f"expecting minute shift component to be an integer"
+                        pc2, "expecting minute shift component to be an integer"
                     )
 
                 maxval = u8.max()
@@ -328,9 +328,9 @@ class Repl:
 
     async def book(self):
         name = await self._prompt_facility()
-        print(HTML(f"<u>Enter <b>start</b> time</u>:"))
+        print(HTML("<u>Enter <b>start</b> time</u>:"))
         start = await self._prompt_time()
-        print(HTML(f"<u>Enter <b>end</b> time</u>"))
+        print(HTML("<u>Enter <b>end</b> time</u>"))
         end = await self._prompt_time()
 
         rpc_tr = dtrange_as_rpc_tr(DateTimeRange(start, end))
@@ -370,9 +370,9 @@ class Repl:
         )
 
     async def swap(self):
-        print(HTML(f"<u>Enter <b>first</b> booking</u>:"))
+        print(HTML("<u>Enter <b>first</b> booking</u>:"))
         bid1 = await self._prompt_bid()
-        print(HTML(f"<u>Enter <b>second</b> booking</u>"))
+        print(HTML("<u>Enter <b>second</b> booking</u>"))
         bid2 = await self._prompt_bid()
 
         res = await self._proxy.swap(String(bid1), String(bid2))

cx4013/rpc/common.py
@@ -8,7 +8,7 @@ Common RPC definitions.
 import inspect
 from typing import Callable, Any, Final
 from typing_extensions import Protocol
-from serialization.common import Serializable
+from cx4013.serialization.common import Serializable
 from abc import ABC
 from collections import Awaitable
 
@@ -37,7 +37,7 @@ def remotemethod(func: Callable) -> Callable:
 
         if name.startswith("_"):
             # todo: maybe check during the decorator?
-            raise ValueError(f"remote method cannot start with _")
+            raise ValueError("remote method cannot start with _")
 
         if parameter.kind != inspect.Parameter.POSITIONAL_OR_KEYWORD:
             raise TypeError(

cx4013/rpc/helpers.py
@@ -8,9 +8,9 @@ Convenience functions for creating RPC servers and clients.
 import asyncio
 import socket
 from typing import Callable, cast, Optional
-from rpc.proxy import Proxy
-from rpc.skeleton import Skeleton
-from rpc.protocol import AddressType, RPCClient, RPCServer
+from cx4013.rpc.proxy import Proxy
+from cx4013.rpc.skeleton import Skeleton
+from cx4013.rpc.protocol import AddressType, RPCClient, RPCServer
 
 
 async def create_server(

commit ed1f0ff007d9c5503b3ffc5cf69d18010feb8701

server: sleep forever instead of calling the event loop to run.
fixes: b16c01a4068250e836c4104a543dde12f33a0e3d

cx4013/server/main.py
@@ -132,8 +132,7 @@ async def main(args: Sequence[str]) -> int:
         parsed.etimeout,
     )
     # Serve continuously.
-    loop = asyncio.get_running_loop()
-    loop.run_forever()
+    await asyncio.sleep(sys.float_info.max)
     s.stop()
 
     return 0